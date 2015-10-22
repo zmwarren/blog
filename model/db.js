8772e87e204c22fa80c05e157c2a67f5941ef3e2
zmwarren <zach@wl-85-147.wireless.umt.edu>
2015-10-22 09:32:20 -0600

    changed db.js

--- model/db.js
+++ model/db.js
@@ -1,3 +1,11 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/blog');
+var uriUtil = require('mongodb-uri');
 
+var options = {
+  server:  { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+};  
+var mongodbUri = process.env.MONGOLAB_URI || "mongodb://localhost/blog";
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri, options);
