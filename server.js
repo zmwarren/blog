e0b64dd3574576e780c1d37f77358b8f5dc57d30
zmwarren <zach@wl-85-147.wireless.umt.edu>
2015-10-22 14:02:59 -0600

    Added login, signup, profile pages with react

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var fs = require('fs');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
+var method = require('method-override');
+var passportLocal = require('passport-local');
 var bodyParser = require('body-parser');
 var db = require('./model/db');
 var Blog = require('./model/blog');
 
 
-
+require('./config/passport')(passport);
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -17,6 +25,18 @@ var blogRoutes = require('./routes/blog');
 app.use(express.static('public'));
 
 app.use('/api/blog', blogRoutes);
+
+app.use(morgan('dev')); 
+app.use(cookieParser()); 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./routes/userRoutes.js')(app, passport);
 
 app.get('/public', function(req, res){
 	res.readFile('blog.html')
